d7bcaa6eeb6840bf2853471d4d1fa5da7fd62579
Trowlett0 <98246324+Trowlett0@users.noreply.github.com>
2024-03-24 15:05:50 -0600

    Fixing canvas

--- SpriteEditor/main.cpp
+++ SpriteEditor/main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    model m;
+    MainWindow w(m);
     w.show();
     return a.exec();
 }
--- SpriteEditor/mainwindow.cpp
+++ SpriteEditor/mainwindow.cpp
@@ -7,15 +7,15 @@ MainWindow::MainWindow(model &m, QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->Canvas->setGridSize(32);
-    QObject::connect(ui->Canvas,
+    ui->canvas->setGridSize(32);
+    QObject::connect(ui->canvas,
             &Canvas::gridClicked,
             &m,
             &model::drawImage);
 
     QObject::connect(&m.f,
             &Frame::updateImage,
-            ui->Canvas,
+                     ui->canvas,
             &Canvas::updateCanvas);
 }
 
