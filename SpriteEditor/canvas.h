67cb4651a2b6a7a6925d362c23de41fb906d316d
Trowlett0 <98246324+Trowlett0@users.noreply.github.com>
2024-04-01 20:52:47 -0600

    Changing commenting consistency

--- SpriteEditor/canvas.h
+++ SpriteEditor/canvas.h
@@ -16,60 +16,79 @@ class Canvas : public QLabel
 {
     Q_OBJECT
 public:
+
     /// \brief Canvas constructor
     /// \param parent the parent widget, default is nullptr
     explicit Canvas(QWidget *parent = nullptr);
+
     /// \brief setGridSize sets the size of the background grid
     /// \param size ex. 16, 32, 64
     void setGridSize(int size);
+
     /// \brief drawGrid draws the grid on the UI canvas
     void drawGrid();
+
     /// \brief layers vector storing the individual layers as QImages
     std::vector<QImage> layers;
 
-//protected:
     /// \brief paintEvent event to capture drawing
     /// \param event sent by UI
     void paintEvent(QPaintEvent *event);
+
     /// \brief mouseMoveEvent event to capture mouse movement
     /// \param event sent by UI
     void mouseMoveEvent(QMouseEvent *event);
+
     /// \brief mousePressEvent event to capture mouse pressed
     /// \param event sent by UI
     void mousePressEvent(QMouseEvent *event);
+
     /// \brief mouseReleaseEvent event to capture mouse released
     /// \param event sent by UI
     void mouseReleaseEvent(QMouseEvent *event);
 
 private:
+
     /// \brief gridSize size of grid on UI canvas
     int gridSize;
+
     /// \brief trackMouse bool to track when the mouse is being tracked
     bool trackMouse;
+
     /// \brief cellSize size of the cells
     int cellSize;
+
     /// \brief startPoint QPoint to store a mouse startpoint
     QPoint startPoint;
+
     /// \brief endPoint QPoint to store a moiuse endpoint
     QPoint endPoint;
 
 
 signals:
+
     /// \brief gridClicked emitted when there is a mouse clicked event
     void gridClicked(QPoint);
+
     /// \brief drawingAction emitted when there is a drawing event
     /// \param startPoint taken from mouse pressed event
     /// \param endPoint taken from mouse released event
     void drawingAction(QPoint startPoint, QPoint endPoint);
+
 public slots:
+
     /// \brief updateCanvas updates the UI canvas based on the sent vector of QImages
     void updateCanvas(std::vector<QImage>);
+
     /// \brief updateGridSize16 updates the UI grid size to 16x16
     void updateGridSizeTo16();
+
     /// \brief updateGridSize32 updates the UI grid size to 32x32
     void updateGridSizeTo32();
+
     /// \brief updateGridSize64 updates the UI grid size to 64x64
     void updateGridSizeTo64();
+
     /// \brief loadPressed displays the dialog box for file loading
     void loadPressed();
 };
--- SpriteEditor/main.cpp
+++ SpriteEditor/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 
+//Reviewed by - Tanner Rowlett
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
